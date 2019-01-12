3ce2207bdc39059aa3bade3eda993a7ac6ba7c3e
droruri <droruri@gmail.com>
2019-01-12 16:56:22 +0200

    change constant

--- src/utilities/constants.ts
+++ src/utilities/constants.ts
@@ -1,4 +1,4 @@
 export const GENERAL_WEBSITE_PREFIX = 'www.';
-export const GENERAL_WEBSITE_SUFFIX = '.maxcard.co.il';
+export const GENERAL_WEBSITE_SUFFIX = '.thexsite.co.il';
 
 export const OK_RESULT = 1;
