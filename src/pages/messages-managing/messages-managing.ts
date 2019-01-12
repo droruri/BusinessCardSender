4a0c0d4644082954bca2fefb42863fcba21bd604
droruri <droruri@gmail.com>
2019-01-12 16:50:51 +0200

    fixing bug of message deleting

--- src/pages/messages-managing/messages-managing.ts
+++ src/pages/messages-managing/messages-managing.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {IonicPage, NavController, NavParams} from 'ionic-angular';
 import {MessagesStorageProvider} from "../../providers/messages-storage/messages-storage";
 import {Message} from "../../models/Message";
 import {Dialogs} from "@ionic-native/dialogs";
@@ -17,26 +17,26 @@ import {OK_RESULT} from "../../utilities/constants";
   selector: 'page-messages-managing',
   templateUrl: 'messages-managing.html',
 })
-export class MessagesManagingPage implements OnInit{
+export class MessagesManagingPage implements OnInit {
 
-  favoriteMessage:Message = null;
-  messagesToSend:Message[] = [];
+  favoriteMessage: Message = null;
+  messagesToSend: Message[] = [];
 
   constructor(private dialogs: Dialogs,
               public navCtrl: NavController,
               public navParams: NavParams,
-              private _messagesStorageProvider:MessagesStorageProvider) {
+              private _messagesStorageProvider: MessagesStorageProvider) {
 
   }
 
   ngOnInit(): void {
     this._messagesStorageProvider.messagesToSendObservable
-      .subscribe((messages:Message[]) => {
+      .subscribe((messages: Message[]) => {
         this.messagesToSend = messages;
       });
 
     this._messagesStorageProvider.favoriteMessageObservable
-      .subscribe((message)=>{
+      .subscribe((message) => {
         this.favoriteMessage = message;
       });
   }
@@ -45,23 +45,25 @@ export class MessagesManagingPage implements OnInit{
     console.log('ionViewDidLoad MessagesManagingPage');
   }
 
-  deleteMessage(message:Message){
-    this.dialogs.confirm(`ההודעה תימחק, האם אתה בטוח?`,'אזהרה').then(result => {
-      if(result === OK_RESULT){
-        this._messagesStorageProvider.deleteMessageById(message.id);
-        if(message.id === this.favoriteMessage.id){
-          this.setFavoriteMessage({id:null, content:''})
-        }
+  deleteMessage(message: Message) {
+    this.dialogs.confirm(`ההודעה תימחק, האם אתה בטוח?`, 'אזהרה').then(result => {
+      if (result === OK_RESULT) {
+        this._messagesStorageProvider.deleteMessageById(message.id)
+          .then(() => {
+            if (message.id === this.favoriteMessage.id) {
+              this.setFavoriteMessage(this.messagesToSend[0])
+            }
+          });
       }
     });
   }
 
-  setFavoriteMessage(message:Message){
-    this._messagesStorageProvider.setFavoriteMessageInLocalStorage(message);
+  setFavoriteMessage(message: Message) {
+    this._messagesStorageProvider.updateFavoriteMessage(message);
   }
 
-  isMessageNotFavorite(message){
-    if (this.favoriteMessage !== null){
+  isMessageNotFavorite(message) {
+    if (this.favoriteMessage !== null) {
       return (this.favoriteMessage.id !== message.id)
     }
     return true;
