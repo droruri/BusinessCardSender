1ca10169f9842d0232ef3996e490223ab8064e07
droruri <droruri@gmail.com>
2019-01-16 22:04:02 +0200

    removing irrelevant dependency

--- src/pages/last-calls-view/last-calls-view.module.ts
+++ src/pages/last-calls-view/last-calls-view.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { LastCallsViewPage } from './last-calls-view';
 import {TranslateModule} from "@ngx-translate/core";
-import {ButtonsSliderComponent} from "../../components/buttons-slider/buttons-slider";
 
 @NgModule({
   declarations: [
-    LastCallsViewPage,
-    ButtonsSliderComponent
+    LastCallsViewPage
   ],
   imports: [
     IonicPageModule.forChild(LastCallsViewPage),
