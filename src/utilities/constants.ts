09cd52101af4d55ecb93450aa8f90c65cf5679df
droruri <droruri@gmail.com>
2019-01-16 22:01:59 +0200

    adding the requested suffix

--- src/utilities/constants.ts
+++ src/utilities/constants.ts
@@ -1,4 +1,5 @@
 export const GENERAL_WEBSITE_PREFIX = 'www.';
 export const GENERAL_WEBSITE_SUFFIX = '.thexsite.co.il';
+export const REFERENCE_ID_SUFFIX = '/?wpam_id=';
 
 export const OK_RESULT = 1;
