70d40f72b181ec29f7878f4fc8631e5b37c0217d
droruri <droruri@gmail.com>
2019-01-19 10:35:39 +0200

    saving all attachments as constants

--- src/utilities/constants.ts
+++ src/utilities/constants.ts
@@ -3,3 +3,11 @@ export const GENERAL_WEBSITE_SUFFIX = '.thexsite.co.il';
 export const REFERENCE_ID_SUFFIX = '/?wpam_id=';
 
 export const OK_RESULT = 1;
+
+export const CHOOSE_PHONE_CALL = 'phone-call';
+export const CHOOSE_SMS = 'sms';
+export const CHOOSE_WHATSAPP = 'whatsapp';
+
+export type attachments = 'phone-call' | 'sms' | 'whatsapp';
+
+
